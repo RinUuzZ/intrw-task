a934248cc4669249d0a0ccb3d70c224e32175d40
RinuZz <rinujasi32@gmail.com>
2023-12-14 18:18:19 +0530

    react-assignment

--- src/App.js
+++ src/App.js
@@ -1,109 +1,168 @@
 import './App.css';
+import goldImage from '../src/assetss/goldimage.png'
 
 function App() {
   return (
     <div className="main">
-      <div className="navbar">
-        <div className="img"></div>
-        <div className="date">
-          <h2>11.24 <span>Am</span></h2>
-          <h4>Monday</h4>
-          <h5>27 MAY 2023</h5>
-        </div>
-      </div>
-
-      <div className="container">
-        <h6>SPOT RATE</h6>
-      </div>
-      <div className="head">
-        <div className="h-six">
-          <h6>Commodity</h6>
-          <h6>Weight</h6>
-          <h6>Buy <sub>AED</sub></h6>
-          <h6>Sell <sub>AED</sub></h6>
-        </div>
-      </div>
-
-      <div className="box">
-        <h3 className='gold'>GOLD</h3>
-        <div className="buy-sell">
-          <div className="buy"><p>$</p><h3>BUYoz</h3></div>
-          <div className="sell"><p>$</p><h3>SELLoz</h3></div>
-        </div>
-
-        <div className="dets">
-          <div className="red">1800.13</div>
-          <div className="green">1800.13</div>
-        </div>
-
-        <div className="low-high">
-          <div className="low">Low</div>
-          <div className="nmbrs">1813.06</div>
-          <div className="high">High</div>
-          <div className="nmbrs-1">1813.06</div>
-        </div>
-
-        <h3 className='gold'>SILVER</h3>
-        <div className="buy-sell">
-          <div className="buy"><p>$</p><h3>BUYoz</h3></div>
-          <div className="sell"><p>$</p><h3>SELLoz</h3></div>
-        </div>
-
-        <div className="dets">
-          <div className="red">1800.13</div>
-          <div className="green">1800.13</div>
-        </div>
-
-        <div className="low-high">
-          <div className="low">Low</div>
-          <div className="nmbrs">1813.06</div>
-          <div className="high">High</div>
-          <div className="nmbrs-1">1813.06</div>
-        </div>
-
-      </div>
-
-      <div className="deatail-section">
-          <div className="contents">
-            <h1>GOLD</h1>
-            <h1>1GM</h1>
-            <h1>217.04</h1>
-            <h1>217.04</h1>
-
-          </div>
-          <div className="contents-1">
-          <h1>GOLD TEN TOLA</h1>
-            <h1>TTB</h1>
-            <h1>25,330</h1>
-            <h1>25,330</h1>
-          </div>
-          <div className="contents-2">
-          <h1>GOLD 995</h1>
-            <h1>1KG</h1>
-            <h1>2,17,331</h1>
-            <h1>2,17,331</h1>
-          </div>
-          <div className="contents-3">
-          <h1>GOLD 9999</h1>
-            <h1>1KG</h1>
-            <h1>2,17,331</h1>
-            <h1>2,17,331</h1>
-          </div>
-          <div className="contents-4">
-          <h1>SILVER</h1>
-            <h1>1KG</h1>
-            <h1>2,17,331</h1>
-            <h1>2,17,331</h1>
-          </div>
-
-      </div>
-
-           <div className="bottom">Updates</div>
-            <div className="bottom-last">
-            st Gold News, Gold rate, Gold price in India, Go Know the latest Gold News, Gold rate, Gold price
+        <div className="navBar">
+           <div className="first-image">
+            <img src={goldImage} alt="" />
+           </div>
+              <div className="date-and-time">
+                <h5 className='time'>11.24 AM</h5>
+                <h6 className='day'>Monday</h6>
+                <h5 className='date'>27 MAY 2023</h5>
               </div>
-           
+        </div>
+        <div className="second-div">
+          <div className="spotRate">
+            <h2>SPOT RATE</h2>
+          </div>
+          <div className="cwbs">
+            <h2>Commodity</h2>
+            <h2>Weight</h2>
+            <h2>Buy </h2><sub>AED</sub>
+            <h2>Sell </h2><sub>SED</sub>
+          </div>
+        </div>
 
+          <div className="gd-slvr-box">
+            <div className="main-box">
+              <div className="gold">
+              <h2>GOLD</h2>
+              </div>
+              <div className="buy-sell">
+                <div className="buy">
+                  <div className="box">$</div>
+                  <div className="text">BUY</div>
+                  <div className="oz">oz</div>
+                </div>
+                <div className="sell">
+                <div className="box">$</div>
+                  <div className="text">BUY</div>
+                  <div className="oz">oz</div>
+                </div>
+              </div>
+
+                  <div className="rates">
+                    <div className="red">
+                      <h2>1800.13</h2>
+                    </div>
+                    <div className="green">
+                      <h2>1800.13</h2>
+                    </div>
+                  </div>
+
+                  <div className="high-low">
+                    <div className="low">
+                      <h4>Low</h4>
+                      <h3> 1813.06</h3>
+                    </div>
+                    <div className="high">
+                      <h4>High</h4>
+                      <h3>1813.06</h3>
+                    </div>
+                  </div>
+
+                    <div className="silver">
+              <h2>SILVER</h2>
+              </div>
+              <div className="buy-selll">
+                <div className="buy">
+                  <div className="box">$</div>
+                  <div className="text">BUY</div>
+                  <div className="oz">oz</div>
+                </div>
+                <div className="sell">
+                <div className="box">$</div>
+                  <div className="text">BUY</div>
+                  <div className="oz">oz</div>
+              </div>
+                    </div>
+
+                    <div className="rates">
+                    <div className="red">
+                      <h2>1800.13</h2>
+                    </div>
+                    <div className="green">
+                      <h2>1800.13</h2>
+                    </div>
+                  </div>
+
+                  <div className="high-low">
+                    <div className="low">
+                      <h4>Low</h4>
+                      <h3> 1813.06</h3>
+                    </div>
+                    <div className="high">
+                      <h4>High</h4>
+                      <h3>1813.06</h3>
+                    </div>
+                  </div>
+
+            </div>
+
+            <div className="boxes">
+              <div className="first-box">
+                <h1>GOLD</h1>
+                <h1>1GM</h1>
+                <h1>217.04</h1>
+                <h1>217.04</h1>
+              </div>
+                <div className="second-box">
+                  <h1>GOLD </h1><h2>TEN TOLA </h2>
+                  <h1 className='ttb'>TTB</h1>
+                  <h1>25,330</h1>
+                  <h1>25,330</h1>
+                </div>
+                   <div className="third-box">
+                    <h1>GOLD</h1> <h2>995</h2>
+                    <h1>1KG</h1>
+                    <h1>2,17,331</h1>
+                    <h1>2,17,331</h1>
+                   </div>
+                      <div className="fourth-box">
+                        <h1>GOLD</h1><h2>9999</h2>
+                        <h1>1KG</h1>
+                        <h1>2,17,331</h1>
+                        <h1>2,17,331</h1>
+                      </div>
+                         <div className="fifth-box">
+                              <h1>SILVER</h1>
+                              <h1>1KG</h1>
+                              <h1>2,17,331</h1>
+                              <h1>2,17,331</h1>
+                          </div>              
+            </div>
+
+          </div>
+
+          <div className="line-head">
+            <div className="heads">
+            <h1 className="buyyers">BUYERS</h1>
+            <h1 className="mesurs">-0.270%</h1>
+            <h1 className="sellers">SELLERS</h1>
+            </div>
+          </div>
+
+          <div className="line">
+            <div className="green-line"></div>
+            <div className="red-line"></div>
+          </div>
+
+          <div className="botom">
+            <div className="frst-prc">62%</div>
+            <div className="scnd-prct">38%</div>
+          </div>
+
+          <div className="updates">
+            <div className="update-text">
+              <h1>Updates</h1>
+            </div>
+            <div className="main-text">
+              <h1>st Gold News, Gold rate, Gold price in India, Go Know the latest Gold News, Gold rate, Gold price </h1>
+            </div>
+          </div>
 
     </div>
 
